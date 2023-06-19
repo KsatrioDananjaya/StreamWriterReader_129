3d3e66bdd81b5e1df22bee49901b607dcd3d2cee
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-06-19 14:46:36 +0700

    membuat outfile dan text

--- iostream throwcatch/iofile.cpp
+++ iostream throwcatch/iofile.cpp
@@ -1,6 +1,16 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string baris;
+
+    //membuka file dalam model menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis File, \ 'q\' untuk keluar" << endl;
 }
