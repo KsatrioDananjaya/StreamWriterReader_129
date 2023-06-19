4292504a1fa708080d32695f2856258c3041b1ca
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-06-19 15:29:36 +0700

    menulis try

--- manualexeption/manualexeption.cpp
+++ manualexeption/manualexeption.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+
 }
