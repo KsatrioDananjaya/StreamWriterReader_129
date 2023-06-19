07152c27431fb5103b83adacfbcb3b3c4e1b0279
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-06-19 15:32:09 +0700

    menambahkan catatan

--- manualexeption/manualexeption.cpp
+++ manualexeption/manualexeption.cpp
@@ -6,15 +6,15 @@ int main()
     try {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
         throw 0.5; //melemparkan sebuah integer maka
-        cout << "Pernyataan tidak akan dieksekusi" << endl;
+        cout << "Pernyataan tidak akan dieksekusi" << endl; //tidak ada throw
     }
     catch (int a) {
         //blok ini akan dieksekusi
-        cout << "Pengecualian akan dieksekusi" << endl;
+        cout << "Pengecualian akan dieksekusi" << endl; //throw bilangan bulat
     }
     catch (...) {
         //jika selain integer maka block ini akan dieksekusi
-        cout << "default Pengecualian akan dieksekusi" << endl;
+        cout << "default Pengecualian akan dieksekusi" << endl; //throw bukan bilangan
     }
     return 0;
 }
