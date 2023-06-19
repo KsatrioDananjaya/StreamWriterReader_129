8bd98f10252d64805f5a4afdd32c96305c6ef66e
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-06-19 15:39:48 +0700

    menambahkan throw dan catch

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -7,5 +7,17 @@ using namespace std;
 
 int main()
 {
-    \
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3>data = { 1, 2, 3 };
+        //pesan array integer 3 elemen
+        cout << data.at(4) << endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        //akan dieksekusi karena array datanya hanya memiliki 3 elemens
+    }
+
 }
